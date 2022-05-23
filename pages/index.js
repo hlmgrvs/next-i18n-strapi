31b7e87169984282bbe602297628a1ce0217a4ca
hlmgrvs <hlmgrvs@gmail.com>
2022-05-23 07:45:43 +0200

    basic i18n setup - language switcher, locale slugs, translation in json

---
+++ pages/index.js
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+    import { useRouter } from 'next/router'
+    
+    import { useTranslation } from 'next-i18next'
+    import { serverSideTranslations } from 'next-i18next/serverSideTranslations'
+    
+    export default function Home({instructions}) {
+      const router = useRouter()
+      const { t } = useTranslation('common')
+      
+      return (
+        <div style={{margin: '20px'}}>
+          <h2>{t('instructions')}</h2>
+          {
+            instructions.length > 0 && instructions.map((instruction) => {
+              return(
+                <div key={`instruction-${instruction.id}`}>
+                  {instruction.attributes.title}
+                </div>
+              )
+            })
+          }
+          <div style={{marginTop: '20px'}}>
+            <Link
+              href='/'
+              locale={router.locale === 'en' ? 'de' : 'en'}>
+                {t('change_locale')}
+            </Link>
+          </div>
+        </div>
+      )
+    }
+    
+    export const getServerSideProps = async ({ locale }) => {
+      const res = await fetch(`http://localhost:1337/api/instructions?locale=${locale}`)
+      //Get your list of instructions
+      const { data } = await res.json()
+    
+      return {
+        props: {
+          instructions: data,
+          ...await serverSideTranslations(locale, ['common']),
+        },
+      }
+    }
